d8d517fa12ed25134dfdcada5a1fff2a3442e7fa
Dominik <Micbrz20@gmail.com>
2023-10-16 22:53:22 +0200

    poprawa - 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,7 @@ android {
 }
 
 dependencies {
-
+    implementation("com.google.firebase:firebase-auth-ktx:22.1.2")
     implementation("com.google.firebase:firebase-bom:32.3.1")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
---
+++ app/src/main/java/com/example/projekt/BaseFragment.java
@@ -0,0 +1,10 @@
+package com.example.projekt;
+
+import androidx.fragment.app.Fragment;
+
+
+abstract class BaseFragment: Fragment() {
+    protected fun startApp(){
+            val intent: = Intent(requireContext())
+        }
+}
