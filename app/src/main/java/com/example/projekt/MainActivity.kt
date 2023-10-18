f2fa2f2e4cafa8821e0286a58de99f08c8b76c95
Dominik <Micbrz20@gmail.com>
2023-10-18 14:41:20 +0200

    fragmenty - 1

--- app/src/main/java/com/example/projekt/MainActivity.kt
+++ app/src/main/java/com/example/projekt/MainActivity.kt
@@ -9,17 +9,19 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        /*
         binding.LogIn.setOnClickListener {
-            val intent = Intent(this, Logged::class.java)
+            val intent = Intent(this, ::class.java)
             startActivity(intent)
         }
         binding.Create.setOnClickListener {
-            val intent = Intent(this, Create::class.java)
+            val intent = Intent(this, ::class.java)
             startActivity(intent)
-        }
+        }*/
 
     }
 }
